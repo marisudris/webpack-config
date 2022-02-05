f382416718d4f780f66641ccb21c8fcfeeb667e5
Māris Ūdris <maris.udris.lv@gmail.com>
2022-02-05 09:45:13 +0200

    Fix react-refresh dev build HMR issue

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ const plugins = [
 
 if (process.env.NODE_ENV === 'production') {
     mode = 'production';
-} else {
+}
+if (process.env.SERVE) {
     plugins.push(new ReactRefresgWebpackPlugin());
 }
 
