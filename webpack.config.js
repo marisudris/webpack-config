d1ab6bbd17403d68e64e1ce913e49402fee89815
Māris Ūdris <maris.udris.lv@gmail.com>
2022-02-05 09:29:32 +0200

    Add react-refresh

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,28 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ReactRefresgWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 let mode = 'development';
 
+const plugins = [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+];
+
 if (process.env.NODE_ENV === 'production') {
     mode = 'production';
+} else {
+    plugins.push(new ReactRefresgWebpackPlugin());
 }
 
 module.exports = {
     mode,
     output: {
-        path: path.resolve(__dirname, 'pub'),
+        path: path.resolve(__dirname, 'dist'),
         assetModuleFilename: 'images/[hash][ext][query]',
     },
     module: {
@@ -39,13 +50,7 @@ module.exports = {
             },
         ],
     },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new CleanWebpackPlugin(),
-    ],
+    plugins,
     devServer: {
         static: './dist',
         hot: true,
